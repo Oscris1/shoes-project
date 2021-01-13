b7166b970fce63bd9ad316573bd0c933ca274d07
Damian Piętka <damianpietka@gmail.com>
2021-01-13 20:37:42 +0100

    created new "magazyn/" directory for magazyn templates

--- magazyn/views.py
+++ magazyn/views.py
@@ -9,7 +9,7 @@ from .filters import ButyFilter
 class MagazynListView(PermissionRequiredMixin, ListView):
     model = Buty
     context_object_name = 'buty'
-    template_name='magazyn_list.html'
+    template_name='magazyn/magazyn_list.html'
     permission_required="magazyn.magazyn_admin"
     
     def get_queryset(self):
@@ -26,20 +26,20 @@ class MagazynListView(PermissionRequiredMixin, ListView):
 class MagazynDetailView(PermissionRequiredMixin, DetailView):
     model = Buty
     context_object_name = 'para'
-    template_name='magazyn_detail.html'
+    template_name='magazyn/magazyn_detail.html'
     permission_required="magazyn.magazyn_admin"
 
 
 class MagazynCreateView(PermissionRequiredMixin, CreateView):
     model = Buty
     fields = "__all__"
-    template_name='magazyn_create.html'
+    template_name='magazyn/magazyn_create.html'
     permission_required="magazyn.magazyn_admin"
 
 
 class MagazynUpdateView(PermissionRequiredMixin, UpdateView):
     model = Buty
-    template_name='magazyn_update.html'
+    template_name='magazyn/magazyn_update.html'
     fields = ['status','szacowana_wartosc', 'uwagi', 'data_sprzedazy', 'cena_sprzedazy']
     permission_required="magazyn.magazyn_admin"
 
@@ -47,6 +47,6 @@ class MagazynUpdateView(PermissionRequiredMixin, UpdateView):
 class MagazynDeleteView(PermissionRequiredMixin, DeleteView):
     model = Buty
     context_object_name = 'para'
-    template_name='magazyn_delete.html'
+    template_name='magazyn/magazyn_delete.html'
     success_url = reverse_lazy('magazyn_list')
     permission_required="magazyn.magazyn_admin"
